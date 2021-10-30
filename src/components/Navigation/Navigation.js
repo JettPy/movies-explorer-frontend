bf810bb3df9407de4072866e27814f253d4aca99
Sergey Melnikov <jettpy@yandex.ru>
2021-10-30 16:27:53 +0300

    feat: add menu in navigation via button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,57 @@
+import React from 'react';
 import avatar from '../../images/default.png';
-import menuIcon from '../../images/menu.svg'
 import { Link } from "react-router-dom";
 import './Navigation.css';
 
 function Navigation() {
 
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   const onOpenMenu = () => {
-    console.log('Тык!');
+    setIsMenuOpen(true);
+  };
+
+  const onClose = () => {
+    setIsMenuOpen(false);
   };
 
   return (
     <nav className="navigation">
-      <ul className="navigation__list list">
-        <li className="navigation__element">
-          <Link to="/movies" className="navigation__link link">Фильмы</Link>
-        </li>
-        <li className="navigation__element">
-          <Link to="/saved-movies" className="navigation__link link">Сохранённые фильмы</Link>
-        </li>
-      </ul>
-      <Link to="/profile" className="navigation__profile link">
-        Аккаунт
-        <img src={avatar} alt="Аватар" className="navigation__avatar" />
-      </Link>
-      <button className="navigation__button button" onClick={onOpenMenu}>
-        <img src={menuIcon} alt="Меню" className="navigation__menu-icon" />
-      </button>
+      <div className="navigation__bar">
+        <ul className="navigation__list list">
+          <li className="navigation__element">
+            <Link to="/movies" className="navigation__link link">Фильмы</Link>
+          </li>
+          <li className="navigation__element">
+            <Link to="/saved-movies" className="navigation__link link">Сохранённые фильмы</Link>
+          </li>
+        </ul>
+        <Link to="/profile" className="navigation__profile link">
+          Аккаунт
+          <img src={avatar} alt="Аватар" className="navigation__avatar" />
+        </Link>
+      </div>
+      <button className={`navigation__button button ${isMenuOpen && "navigation__button_disable"}`} onClick={onOpenMenu}></button>
+      <div className={`navigation__overlay ${isMenuOpen && "navigation__menu_opened"}`}>
+        <div className="navigation__menu">
+          <ul className="navigation__menu-list list">
+            <li className="navigation__menu-element">
+              <Link to="/" className="navigation__link link" onClick={onClose}>Главная</Link>
+            </li>
+            <li className="navigation__menu-element">
+              <Link to="/movies" className="navigation__link link" onClick={onClose}>Фильмы</Link>
+            </li>
+            <li className="navigation__menu-element">
+              <Link to="/saved-movies" className="navigation__link link" onClick={onClose}>Сохранённые фильмы</Link>
+            </li>
+          </ul>
+          <Link to="/profile" className="navigation__profile link" onClick={onClose}>
+            Аккаунт
+            <img src={avatar} alt="Аватар" className="navigation__avatar" />
+          </Link>
+          <button className="navigation__close button" aria-label="Закрыть" type="button" onClick={onClose}></button>
+        </div>
+      </div>
     </nav>
   );
 }
